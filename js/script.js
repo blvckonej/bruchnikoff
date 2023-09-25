28d781acf9178aaeb1723770746939bbae369ab2
Maxim Razin <blvckonej@gmail.com>
2023-09-26 00:08:50 +0300

    edit burger menu

--- js/script.js
+++ js/script.js
@@ -3,3 +3,22 @@ document.addEventListener('DOMContentLoaded', function () {
         document.querySelector('.header__wrapper').classList.toggle('open');
     })
 })
+
+window.addEventListener('keydown', (e) => {
+    if (e.key === "Escape") {
+        document.querySelector('.header__wrapper').classList.remove('open');
+    }
+})
+
+document.getElementById('menu').addEventListener('click', event => {
+    event._isClickWithInMenu = true;
+})
+
+document.getElementById('burger').addEventListener('click', event => {
+    event._isClickWithInMenu = true;
+})
+
+document.body.addEventListener('click', event => {
+    if (event._isClickWithInMenu) return;
+    document.querySelector('.header__wrapper').classList.remove('open')
+})
